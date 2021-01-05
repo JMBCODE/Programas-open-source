b7cb5f2e0b7506c12cc088cc46448ab88f726644
JMBCODE <72136857+JMBCODE@users.noreply.github.com>
2021-01-05 14:20:07 -0300

    Update gravando_arquivos.cpp

--- gravando_arquivos.cpp
+++ gravando_arquivos.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main()
 {
     cout << "Esse programa criara um arquivo chamado text.txt na mesma pasta do programa" << endl;
-    ofstream MyFile("text.txt"); //cria o arquivo para escrevermos nele
+    ofstream MyFile("text.txt"); //cria o arquivo para escrevermos nele, como não passei o caminho, só o nome, então o arquivo ficará na mesma pasta do programa
     MyFile << "Isso foi feito em C++!\nArquivos são legais!";  //escreve no arquivo
     MyFile.close();  //fecha o arquivo
 
